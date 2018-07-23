76859775a2aeb15ade15e880a4762112a7e1f918
lslqtz <admin@acgn.xyz>
2018-07-23 23:56:07 +0800

    !br 指令上线

--- bot.php
+++ bot.php
@@ -1016,6 +1016,29 @@ function PublicCommands($isGroup,$splitarr,$messagearr,$messagecount,&$text) {
 				break;
 			}
 			$userid=GetUserIDByUsername($username);
+			list($scoreID,$beatmapID,$rank,$mods,$finalpp)=$conn->queryRow("SELECT score_id, beatmap_id, rank, enabled_mods, pp FROM osu_scores_high WHERE user_id = {$userid} ORDER BY date DESC LIMIT 1",1);
+			if (empty($rank)) {
+				$text.="No play records.\n";
+				break;
+			}
+			$pp=$conn->queryOne("SELECT pp FROM osu_scores WHERE score_id = {$scoreID} LIMIT 1");
+			$pp=sprintf('%.2f',$pp);
+			$finalpp=sprintf('%.2f',$finalpp);
+			$rank=str_replace('H','+Hidden',str_replace('X','SS',$rank));
+			$mods=getShortModString($mods,0);
+			$text.="{$username}'s BanYou Recent (osu!)\n";
+			list($hit_length,$total_length,$beatmap_name,$beatmap_version)=$conn->queryRow("SELECT hit_length, total_length, CONCAT(IF(artist != '',CONCAT(artist,' - ',title),title)), version FROM osu_beatmaps WHERE beatmap_id = {$beatmapID} LIMIT 1",1);
+			if (!empty($beatmap_name)) {
+				$text.="{$beatmap_name} [{$beatmap_version}]\n";
+			}
+			$text.="评级：{$rank}，Mods：{$mods}，{$pp}pp({$finalpp}pp)\n";
+			$text.="谱面：https://osu.ppy.sh/b/{$beatmapID}";
+			if (!empty($hit_length)) {
+				$text.="，谱长：{$hit_length} 秒，总长：{$total_length} 秒";
+			}
+			$text.="\n";
+			$text.="用户页：https://user.osupink.net/".rawurlencode($username);
+			$text.="\n";
 			break;
 		default:
 			return 0;
