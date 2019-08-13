1b3e69dd91d68cf8bc4dc342c93416a8985d717d
lslqtz <admin@acgn.xyz>
2019-08-14 06:32:43 +0800

    bug 修复，性能优化

--- bot.php
+++ bot.php
@@ -331,6 +331,11 @@ function CheckEvent() {
 		flock($lastEventIDFile,LOCK_EX);
 		$lastEventID=fgets($lastEventIDFile);
 		$eventList=$conn->queryAll("SELECT e.id, e.mode as mode, m.modename as modename, e.user_id as user_id, u.username as username, e.beatmap_id as beatmap_id, b.beatmapset_id as beatmapset_id, e.text as ranknumber, CONCAT(IF(b.artist != '',CONCAT(b.artist,' - ',b.title),b.title)) as beatmap_name, b.version as version, b.hit_length as hit_length, b.total_length as total_length, REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(et.`zh-ubbrule`,'{user_id}',e.user_id),'{username}',u.username),'{text}',e.text),'{beatmap_id}',e.beatmap_id),'{mode}',e.mode),'{artist}',IF(b.artist != '',CONCAT(b.artist,' - '),'')),'{title}',b.title),'{version}',b.version),'{modename}',m.modename) as text FROM osu_events e JOIN osu_users u USING (user_id) JOIN osu_beatmaps b USING (beatmap_id) JOIN osu_events_type et USING (type) JOIN osu_modes m ON m.id = e.mode WHERE e.type = 1 AND e.id > {$lastEventID} ORDER BY e.id");
+		if (count($eventList) < 1) {
+			flock($lastEventIDFile, LOCK_UN);
+			fclose($lastEventIDFile);
+			return;
+		}
 		foreach ($eventList as $value) {
 			setGameMode($value['mode']);
 			list($scoreID,$rank,$modsnumber,$finalpp,$score)=$conn->queryRow("SELECT score_id, rank, enabled_mods, pp, score FROM {$highScoreTable} WHERE user_id = {$value['user_id']} AND beatmap_id = {$value['beatmap_id']} LIMIT 1",1);
@@ -370,6 +375,7 @@ function CheckEvent() {
 		}
 	}
 	$lastEventID=$conn->queryOne("SELECT id FROM osu_events ORDER BY id DESC LIMIT 1");
+	rewind($lastEventIDFile);
 	fwrite($lastEventIDFile, $lastEventID);
 	flock($lastEventIDFile, LOCK_UN);
 	fclose($lastEventIDFile);
