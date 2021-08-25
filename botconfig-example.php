10aa31d85c3f42ef43ec6835f7944ec85c78009b
lslqtz <admin@acgn.xyz>
2021-08-25 15:00:27 +0800

    u

---
+++ botconfig-example.php
@@ -0,0 +1,23 @@
+<?php
+define('BotKey', '');
+define('ClientAccKey', '');
+define('WeatherAPIKey', '');
+define('osuAPIKey','');
+define('DbAddress', '');
+define('DbUsername', '');
+define('DbPassword', '');
+define('DbName', '');
+define('DEBUG', false);
+define('RootPath', __DIR__);
+define('BanYouDomain', 'osu.pink');
+define('MaxFriendsCount', 32);
+define('APIURL', '');
+define('selfQQ', 2487084757);
+define('masterQQ', 2143585062);
+define('devGroupNumber', 609602961);
+define('mainGroupNumber', 686469603);
+#define('groupNumberList', array(devGroupNumber, mainGroupNumber, 132783429));
+define('groupNumberList', array(devGroupNumber, mainGroupNumber, 193067962));
+define('disableNotificationGroupNumberList', array(devGroupNumber, 193067962));
+define('byPrefixGroupNumberList', array(193067962));
+?>
